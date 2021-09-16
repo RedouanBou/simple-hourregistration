58d51711b1d0481db21e55d11e73b32d3e13465c
RedouanBou <76223114+RedouanBou@users.noreply.github.com>
2021-09-16 15:31:49 +0200

    Changed urls in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,11 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
             <div className="container">
-                <Link className="navbar-brand" to="/">Uren Registratie</Link>
+                <Link className="navbar-brand" to="/simple-hourregistration">Uren Registratie</Link>
                 <div>
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li className="nav-item mt-1">
-                            <Link className="btn btn-primary btn-sm" to="/add">Add</Link>
+                            <Link className="btn btn-primary btn-sm" to="/simple-hourregistration/add">Add</Link>
                         </li>
                     </ul>
                 </div>
